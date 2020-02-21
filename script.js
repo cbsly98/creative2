e7e207993f051df5555a548a71c111f13c3a30f2
Caleb Sly <slycaleb@gmail.com>
2020-02-21 15:36:13 -0700

    jokes functionality

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 function onClick(e) {
   e.preventDefault();
 
+  let s = document.getElementById('selector');
+  let type = s.options[s.selectedIndex].value;
+
   // setup URL
-  let url = "https://pokeapi.co/api/v2/" + "pokemon" + "/" + "ditto/";
+  let url = "https://official-joke-api.appspot.com/jokes/" + type + "/random";
   // call API
   fetch(url)
     .then(function(response) {
       // make sure the request was successful
       if (response.status != 200) {
         return {
-          text: "Error calling the Numbers API service: " + response.statusText
+          text: "Error calling the joke API service: " + response.statusText
         }
       }
       return response.json();
     }).then(function(json) {
       // update DOM with response
-      updateResult(json.name);
+      updateResult(json[0].setup);
+      updatePunchline(json[0].punchline);
     });
 }
 
 function updateResult(info) {
-  document.getElementById('results').textContent = info;
+  document.getElementById('joke').textContent = info;
 }
 
-document.getElementById('test').addEventListener('click', onClick);
+function updatePunchline(info) {
+  document.getElementById('punchline').style.visibility = "hidden";
+  document.getElementById('punchline').textContent = info;
+}
+
+function show() {
+  document.getElementById('punchline').style.visibility = "visible";
+}
+
+document.getElementById('getJoke').addEventListener('click', onClick);
+document.getElementById('getPunchline').addEventListener('click', show);
